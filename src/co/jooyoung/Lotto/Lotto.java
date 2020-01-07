76cd7a74109fd3980a52d8f02c0e726e9978df66
JOO <JOO@DESKTOP-R2ICKPJ>
2020-01-08 00:04:20 +0900

    휴.......로도스이ㅑㅇ

--- src/co/jooyoung/Lotto/Lotto.java
+++ src/co/jooyoung/Lotto/Lotto.java
@@ -1,33 +1,44 @@
 package co.jooyoung.Lotto;
 
 public class Lotto {
-	private int money;
+
 	int balls[] = new int[6];
+	int money = 0;
+	int n = money / 1000;
+
 	Lotto() {
 	}
 
-	public int getMoney() {
-		return money;
-	}
+	public void run() {
 
-	public void setMoney(int money) {
-		this.money = money;
-	}
-
-	private void runLotto() {
-		
-		int n = money / 1000;
-		for (int i = 0; i < n; i++) {
-
-			for (int x = 0; x < 6; x++) {
-				balls[x] = (int) (Math.random() * 45) + 1;
-				for (int k = 0; k < x; k++) {
-					if (balls[x] == balls[k]) {
-						x++;
+		for (int i = 0; i < (money / 1000); i++) {
+			for (int j = 0; j < 6; j++) {
+				balls[j] = (int) (Math.random() * 45) + 1;
+				for (int k = 0; k < j; k++) {
+					if (balls[j] == balls[k]) {
+						balls[j]--;
+						if (balls[j] == 0) {
+							balls[j] = (int) (Math.random() * 45) + 1;
+						}
 					}
 				}
-				System.out.print(balls);
+				System.out.print(balls[j] + "  ");
 			}
+			System.out.println();
 		}
+		System.out.println("----------------");
+	}
+
+	public void print() {
+		for (int i = 0; i < (money / 1000); i++) {
+			for (int j = 0; j < 6; j++) {
+				System.out.print(balls[j] + "  ");
+
+			}
+			System.out.println(" ");
+
+		}
+		System.out.println(balls[3]);
+		System.out.println("====================");
 	}
 }
--- src/co/jooyoung/Lotto/MainApp.java
+++ src/co/jooyoung/Lotto/MainApp.java
@@ -6,11 +6,17 @@ public class MainApp extends Lotto{
 	
 	public static void main(String[] args) {
 	Scanner scanner = new Scanner(System.in);
+	Lotto game = new Lotto();
+	
 	
 		System.out.println("금액을 입력하세요 : ");
-		int money = super.money;
-		a = scanner.nextInt();
+		game.money = scanner.nextInt();
 		scanner.nextLine();
+		
+			
+		game.run();
+		///game.print();
+		
 		
 		
 		
